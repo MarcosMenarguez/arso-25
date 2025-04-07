9be934cf6eecb6e0741d47f176a214fe649bde7b
mdclg <48884482+mdclg@users.noreply.github.com>
2025-04-07 15:45:43 +0200

    Semana 12: Bookle en .NET

---
+++ BookleApi/Models/BookleRepositorio.cs
@@ -0,0 +1,59 @@
+
+using Repositorio;
+using MongoDB.Driver;
+using Bookle.Modelo;
+
+namespace Bookle.Repositorio
+{
+
+
+    public class RepositorioActividadesMongoDB : IRepositorio<Actividad, string>
+    {
+        private readonly IMongoCollection<Actividad> actividades;
+
+        public RepositorioActividadesMongoDB()
+        {
+            var client = new MongoClient("uri");
+            var database = client.GetDatabase("arso2025");
+
+            actividades = database.GetCollection<Actividad>("actividades.net");
+        }
+
+        public string Add(Actividad entity)
+        {
+            actividades.InsertOne(entity);
+
+            return entity.Id;
+        }
+
+        public void Update(Actividad entity)
+        {
+            actividades.ReplaceOne(actividad => actividad.Id == entity.Id, entity);
+        }
+
+        public void Delete(Actividad entity)
+        {
+            actividades.DeleteOne(actividad => actividad.Id == entity.Id);
+        }
+
+        public List<Actividad> GetAll()
+        {
+            return actividades.Find(_ => true).ToList();
+        }
+
+        public Actividad GetById(string id)
+        {
+            return actividades
+                .Find(actividad => actividad.Id == id)
+                .FirstOrDefault();
+        }
+
+        public List<string> GetIds()
+        {
+            var todas =  actividades.Find(_ => true).ToList();
+
+            return todas.Select(p => p.Id).ToList();
+
+        }
+    }
+}
